a5781539aec35e7baca76ee039bdfc94f12299d2
Dennis Payk <payk@outlook.de>
2015-09-01 12:49:36 +0200

    Initial Commit

---
+++ bundle/nls/bundle.js
@@ -0,0 +1,21 @@
+define({
+    root: {
+        bundleName: "Elevation Profile",
+        bundleDescription: "This bundle can create an elevation profile",
+        toolName: "Elevation Profile Tool",
+        toolTooltip: "Elevation Profile Tool",
+        windowTitle: "Elevation Profile",
+        digitize: "Digitize a route:",
+        selectunit: "Select unit measure:",
+        polyline: "Polyline",
+        freehandpolyline: "Freehand Polyline",
+        clear: "Clear Profile",
+        miles: "Miles",
+        kilometers: "Kilometers",
+        meters: "Meters",
+        nauticalmiles: "Nautical Miles",
+        yards: "Yards",
+        feet: "Feet"
+    },
+    de: true
+});
