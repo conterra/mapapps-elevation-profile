a5519bd6059fe777757e490d1822218057325673
Matthias Stein <m.stein@conterra.de>
2017-12-06 10:00:35 +0100

    Update inceptionYear

--- src/main/js/bundles/dn_elevationprofile/ProfileChart.js
+++ src/main/js/bundles/dn_elevationprofile/ProfileChart.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 define(["dojo/_base/lang", "dojo/on", "dojo/mouse", "dojo/_base/array", "dojo/number", "dojo/dom-geometry", "dojo/aspect", "esri/dijit/ElevationProfile/ProfileChart", "esri/sniff", "dojox/charting/action2d/MouseIndicator", "dojox/charting/action2d/TouchIndicator"],
     function (d_lang, on, d_mouse, d_array, d_number, dom_geometry, d_aspect, ProfileChart, sniff, MouseIndicator, TouchIndicator) {
 
